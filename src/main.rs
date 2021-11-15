d453173e42f118979fd92b0183190f7976bd942e
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-11-16 01:39:41 +0200

    Pull opts from main

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,23 @@
 #[macro_use]
 extern crate anyhow;
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
-use std::path::PathBuf;
-
-use clap::Parser;
 use anyhow::Result;
+use clap::Parser;
 
-use crate::errors::AldaError;
 use crate::layout::Layout;
+use crate::opts::{Opts, SubCommand};
 
 mod errors;
 mod fetch;
 mod layout;
+mod opts;
 mod torrent;
-
-/// This doc string acts as a help message when the user runs '--help'
-/// as do all doc strings on fields
-#[derive(Parser, Debug)]
-#[clap(about, version)]
-struct Opts {
-    /// Directory with torrent files
-    #[clap(short, long, parse(from_os_str))]
-    torrents: PathBuf,
-
-    /// Directory with saved content
-    #[clap(short, long, parse(from_os_str))]
-    source: PathBuf,
-
-    /// Layout type
-    #[clap(short, long, possible_values = ["simple", "nested"], default_value = "simple")]
-    layout: Layout,
-
-    #[clap(subcommand)]
-    subcommand: SubCommand,
-}
-
-#[derive(Parser, Debug)]
-enum SubCommand {
-    Inspect(Inspect),
-    Move(Move),
-    Cleanup(Cleanup),
-}
-
-/// Look for orphaned files without torrents
-#[derive(Parser, Debug)]
-struct Inspect {}
-
-/// Move orphaned files to separate directory
-#[derive(Parser, Debug)]
-struct Move {
-    /// Where to move orphaned files
-    #[clap(short, long)]
-    destination: String,
-    /// Don't ask for confirmation
-    #[clap(short, long)]
-    force: bool,
-}
-
-/// Delete orphaned files
-#[derive(Parser, Debug)]
-struct Cleanup {
-    /// Don't ask for confirmation
-    #[clap(short, long)]
-    force: bool,
-}
 
 fn main() -> Result<()> {
     simple_logger::init_with_env().unwrap();
@@ -90,17 +40,4 @@ fn main() -> Result<()> {
             Ok(())
         }
     }
-    // match &result {
-    //     Ok(_) => {
-    //         info!("Done!")
-    //     }
-    //     Err(Error::Io(why)) => {
-    //         error!("I/O error occured: {}", why)
-    //     }
-    //     Err(Error::SerDe(why)) => {}
-    //     Err(Error::Traverse(why)) => {
-    //         error!("Can't traverse directory: {}", why)
-    //     }
-    //     Err(Error::Unknown) => {}
-    // }
 }
---
+++ src/opts.rs
@@ -0,0 +1,54 @@
+use std::path::PathBuf;
+
+use crate::Layout;
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Parser, Debug)]
+#[clap(about, version)]
+pub(crate) struct Opts {
+    /// Directory with torrent files
+    #[clap(short, long, parse(from_os_str))]
+    pub(crate) torrents: PathBuf,
+
+    /// Directory with saved content
+    #[clap(short, long, parse(from_os_str))]
+    pub(crate) source: PathBuf,
+
+    /// Layout type
+    #[clap(short, long, possible_values = ["simple", "nested"], default_value = "simple")]
+    pub(crate) layout: Layout,
+
+    #[clap(subcommand)]
+    pub(crate) subcommand: SubCommand,
+}
+
+#[derive(Parser, Debug)]
+pub(crate) enum SubCommand {
+    Inspect(Inspect),
+    Move(Move),
+    Cleanup(Cleanup),
+}
+
+/// Look for orphaned files without torrents
+#[derive(Parser, Debug)]
+pub(crate) struct Inspect {}
+
+/// Move orphaned files to separate directory
+#[derive(Parser, Debug)]
+pub(crate) struct Move {
+    /// Where to move orphaned files
+    #[clap(short, long)]
+    pub(crate) destination: String,
+    /// Don't ask for confirmation
+    #[clap(short, long)]
+    pub(crate) force: bool,
+}
+
+/// Delete orphaned files
+#[derive(Parser, Debug)]
+pub(crate) struct Cleanup {
+    /// Don't ask for confirmation
+    #[clap(short, long)]
+    pub(crate) force: bool,
+}
