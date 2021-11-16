533bea2a82b4087fece21a8980fa079d789b8b81
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-11-16 02:46:37 +0200

    Replace logger, added colors

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate anyhow;
 #[macro_use]
 extern crate clap;
+extern crate colorful;
 #[macro_use]
 extern crate log;
 #[macro_use]
@@ -9,18 +10,30 @@ extern crate serde_derive;
 
 use anyhow::Result;
 use clap::Parser;
+use env_logger::Builder;
+use log::LevelFilter;
 
 use crate::layout::Layout;
 use crate::opts::{Opts, SubCommand};
 
-mod errors;
 mod actions;
+mod errors;
 mod layout;
 mod opts;
 mod torrent;
 
+fn init_logging() {
+    let mut builder = Builder::new();
+    builder.filter_level(LevelFilter::Debug);
+    builder.format_timestamp(None);
+    builder.format_target(false);
+    builder.parse_env("ALDA_LOG");
+    builder.init();
+}
+
 fn main() -> Result<()> {
-    simple_logger::init_with_env().unwrap();
+    init_logging();
+
     let opts: Opts = Opts::parse();
     trace!("{:#?}", opts);
 
